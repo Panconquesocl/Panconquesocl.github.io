25a3757ed8427b82c6b2ee58d1720d4f01804734
Panconquesocl <maxcortesv@gmail.com>
2023-04-12 03:19:11 -0400

    Web Base creada y Funcion gold hen agregada

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("GHen-PS4").addEventListener("click", function() {
+    window.location.href = "https://panconquesocl.github.io/PS4-GoldHenEnabler/"; // URL a la que se redirige
+});
